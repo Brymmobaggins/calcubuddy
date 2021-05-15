b380009293f09d084b560bf2d1cfabc286f2a0f6
Brymmobaggins <bakareibrahim98@gmail.com>
2021-05-15 02:47:11 +0100

    update project

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
 function move() {
-  var elem = document.getElementById("myTv");   
-  var width = 10;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
-      document.getElementById("call").innerHTML = width * 1  + '%';
+    // var value = 
+    var elem = document.getElementById("myBar");
+    var width = 20;
+    var id = setInterval(frame, 800);
+    function frame() {
+        if (width >= 80) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+            document.getElementById("load").innerHTML = width / 2 * 3 + '';
+            document.getElementById("call").innerHTML = width * 1 - 13 + '';
+        }
     }
-  }
 }
 
