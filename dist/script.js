d11616a66b99855121990bc7bfe8575f2a4b8895
brymmobaggins <ibro2tablet21@yahoo.com>
2023-08-21 11:19:00 +0100

    add hidden class to hide the navbar when the page refreshes

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,22 @@
-// console.log("hello word")
-const menu = document.getElementsByClassName("toggle-button")[0]
 
-menu.addEventListener('click', function () {
+const menuToggle = document.getElementsByClassName("toggle-button")[0]
+const pixelInputEl = document.getElementById('px-input')
+const remInputEl = document.getElementById('rem-input')
+
+menuToggle.addEventListener('click', function () {
     let classList = document.getElementById('nav').classList
     classList.toggle('hidden')
     classList.toggle('block')
+})
+
+
+// listen for input on pixel input element
+pixelInputEl.addEventListener('input', () => {
+ 
+    // get pixel value from input
+    let inputPixelValue = pixelInputEl.value 
+
+    // convert pixel to rem (i rem = 16 pixels)
+    remInputEl.value = inputPixelValue * 16
+
 })
