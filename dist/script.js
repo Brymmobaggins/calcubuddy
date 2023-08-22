c791d5abc41db1ccacd607166daf92038dd5102f
brymmobaggins <ibro2tablet21@yahoo.com>
2023-08-22 20:45:10 +0100

    add layer base on global css

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,4 @@
-
+// 
 const menuToggle = document.getElementsByClassName("toggle-button")[0]
 const pixelInputEl = document.getElementById('px-input')
 const remInputEl = document.getElementById('rem-input')
@@ -6,17 +6,45 @@ const remInputEl = document.getElementById('rem-input')
 menuToggle.addEventListener('click', function () {
     let classList = document.getElementById('nav').classList
     classList.toggle('hidden')
-    classList.toggle('block')
+    // classList.toggle('block')
 })
 
+
+const convertRemToPixel = (rem) => {
+    return rem * 16
+}
+const convertPixelToRem = (pixel) => {
+    return pixel / 16
+
+
+}
 
 // listen for input on pixel input element
 pixelInputEl.addEventListener('input', () => {
- 
+
     // get pixel value from input
-    let inputPixelValue = pixelInputEl.value 
+    let pixelValue = pixelInputEl.value
 
     // convert pixel to rem (i rem = 16 pixels)
-    remInputEl.value = inputPixelValue * 16
+    remInputEl.value = convertPixelToRem(pixelValue)
+    addColor()
+    // if(pixelInputEl.oninput){
+    //     remInputEl.classList.add("active")
+    // }
 
 })
+
+remInputEl.addEventListener('input', () => {
+    let remValue = remInputEl.value
+
+    pixelInputEl.value = convertPixelToRem(remValue)
+    addColor()
+
+
+})
+function addColor() {
+    if (pixelInputEl.oninput) {
+        remInputEl.classList.add("active")
+
+    }
+}
